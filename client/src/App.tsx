8d933dced8103dd9f9925f225e82351bf14a6403
Halim Choukani <halim.choukani@gmail.com>
2025-03-01 19:16:31 +0100

    refactor: migrate project to TypeScript and update configurations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>click</Button>
+      <Button>Click me</Button>
     </>
   );
 }
