5259e423cfd44ced277d90019657c4150ba4ffe7
Halim Choukani <halim.choukani@gmail.com>
2025-04-09 18:20:50 +0100

    feat: enhance ProjectPage to fetch project data on ID change

--- client/src/pages/Main/Project.tsx
+++ client/src/pages/Main/Project.tsx
@@ -4,6 +4,7 @@ import { ProjectTabs } from "@/components/projects/project-tabs";
 import { useNavigate, useParams } from "react-router-dom";
 import useGetProjectById from "../../hooks/useGetProjectById";
 import useProject from "../../hooks/useProject";
+import { useEffect } from "react";
 
 export default function ProjectPage() {
   const navigate = useNavigate();
@@ -11,7 +12,12 @@ export default function ProjectPage() {
   if (!id) {
     return;
   }
-  const { project, isLoading, error, setProject } = useProject();
+  const { project, isLoading, error, setProject, getProjectById } =
+    useProject();
+
+  useEffect(() => {
+    getProjectById(id);
+  }, [id]);
 
   if (error) {
     return navigate(-1);
