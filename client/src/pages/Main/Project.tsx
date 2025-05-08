04c0951d6f423d3bae296126a6a8e97f52a30585
Halim Choukani <halim.choukani@gmail.com>
2025-05-08 06:53:34 +0100

    feat: Update ProjectPage to enhance AI interaction button and improve layout

--- client/src/pages/Main/Project.tsx
+++ client/src/pages/Main/Project.tsx
@@ -5,7 +5,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import useGetProjectById from "../../hooks/useGetProjectById";
 import useProject from "../../hooks/useProject";
 import { useEffect, useState } from "react";
-import { MessageCircle } from "lucide-react";
+import { MessageCircle, Sparkle, Sparkles } from "lucide-react";
 
 export default function ProjectPage() {
   const navigate = useNavigate();
@@ -43,12 +43,13 @@ export default function ProjectPage() {
         setProject={setProject}
         isLoading={isLoading}
       />
-      <div className="fixed bottom-4 right-4 z-999 bg-primary rounded-full shadow-xl p-1 transition-transform duration-300 ease-in-out hover:scale-105">
-        <Link to={`/ask-ai/${id}`} className="flex items-center gap-2 p-2">
-          <MessageCircle className="text-white" />
-          <h1 className="text-xl font-bold text-white">Ask AI</h1>
-        </Link>
-      </div>
+      {!isLoading && (
+        <div className="fixed bottom-4 left-4 z-999 bg-primary rounded-full shadow-xl p-1 transition-transform duration-300 ease-in-out hover:scale-105 hover:content-[ASK-AI]">
+          <Link to={`/ask-ai/${id}`} className="flex items-center gap-2 p-2">
+            <Sparkles className="text-white" />
+          </Link>
+        </div>
+      )}
     </>
   );
 }
