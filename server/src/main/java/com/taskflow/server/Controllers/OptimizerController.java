45fe1806b5edf2ff568ffec3823fbd3e1f3f6168
Halim Choukani <halim.choukani@gmail.com>
2025-05-21 08:07:24 +0100

    feat: Implement OptimizerController for project optimization with AI service integration

---
+++ server/src/main/java/com/taskflow/server/Controllers/OptimizerController.java
@@ -0,0 +1,37 @@
+package com.taskflow.server.Controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.taskflow.server.Config.JWT;
+import com.taskflow.server.Entities.Project;
+import com.taskflow.server.Services.AIChatService;
+import com.taskflow.server.Services.OptimiserService;
+import com.taskflow.server.Services.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/optimise")
+@RequiredArgsConstructor
+public class OptimizerController {
+    @Autowired
+    private OptimiserService optimiserService;
+
+
+    @Autowired
+    private JWT myJWT;
+
+
+    @PostMapping(value = "/")
+    public ResponseEntity<?> streamChat(
+            @RequestHeader("Authorization") String token,
+            @RequestBody Map<String, String> requestBody) throws InterruptedException, JsonProcessingException {
+        String projectId = (String)requestBody.get("projectID");
+        ObjectNode optimizerRequest = optimiserService.optimise(projectId);
+        return ResponseEntity.ok(optimizerRequest);
+    }
+}
