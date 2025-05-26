9a23fc52e305055746a60cf76b7fb837b6bd0c1b
lounissi_montassar <montassar9000@gmail.com>
2025-05-26 08:55:59 +0100

    optimisation 5G

---
+++ server/src/main/java/com/taskflow/server/Entities/DTO/ProjectsStatsDTO.java
@@ -0,0 +1,12 @@
+package com.taskflow.server.Entities.DTO;
+
+import lombok.Data;
+
+@Data
+public class ProjectsStatsDTO {
+    private int projects;
+    private int notStartedProjects;
+    private int activeProjects;
+    private int completedProjects;
+    private int createdThisMonth;
+}
--- server/src/main/java/com/taskflow/server/Repositories/ProjectRepository.java
+++ server/src/main/java/com/taskflow/server/Repositories/ProjectRepository.java
@@ -1,6 +1,9 @@
 package com.taskflow.server.Repositories;
 import com.taskflow.server.Entities.Project;
 import com.taskflow.server.Entities.User;
+import com.taskflow.server.Entities.DTO.ProjectsStatsDTO;
+
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,36 @@ public interface ProjectRepository extends MongoRepository<Project, String> {
     public Project getProjectById(String id);
     public Project getProjectByCreateur(User id);
     public List<Project> findAllByStatus(Project.Status status);
+
+        @Aggregation(pipeline = {
+        // 1) group over _all_ projects
+        "{ '$group': {"
+          + "  '_id': null,"
+          + "  'projects':           { '$sum': 1 },"
+          + "  'notStartedProjects':{ '$sum': { '$cond': [ { '$eq': [ '$status', 'NOT_STARTED' ] }, 1, 0 ] } },"
+          + "  'activeProjects':    { '$sum': { '$cond': [ { '$eq': [ '$status', 'IN_PROGRESS' ] }, 1, 0 ] } },"
+          + "  'completedProjects': { '$sum': { '$cond': [ { '$eq': [ '$status', 'COMPLETED'   ] }, 1, 0 ] } },"
+          + "  'createdThisMonth':  { '$sum': {"
+          + "      '$cond': ["
+          + "        { '$and': ["
+          + "            { '$eq': [ { '$year': '$dateCreation' }, { '$year':  '$$NOW' } ] },"
+          + "            { '$eq': [ { '$month':'$dateCreation' }, { '$month':'$$NOW' } ] }"
+          + "        ] },"
+          + "        1, 0"
+          + "      ]"
+          + "    }"
+          + "  }"
+          + "} }",
+        // 2) drop the _id field so it maps cleanly
+        "{ '$project': {"
+          + "  '_id': 0,"
+          + "  'projects': 1,"
+          + "  'notStartedProjects': 1,"
+          + "  'activeProjects': 1,"
+          + "  'completedProjects': 1,"
+          + "  'createdThisMonth': 1"
+          + "} }"
+    })
+    ProjectsStatsDTO getStatsAllProjects();
 
 }
